073d2c4abb45bcd6c402194a2781725a17c2033c
Alexandr Kior <kiordev@gmail.com>
2024-03-23 16:21:24 +0200

    123

--- Basic.cs
+++ Basic.cs
@@ -7,7 +7,7 @@ public class Basic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Some com
     }
 
     // Update is called once per frame
